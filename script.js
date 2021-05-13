ea4404c9c6f7e95a03d993072552e63d2e06bd72
tomoki mori <mori-tomoki@g.ecc.u-tokyo.ac.jp>
2021-05-13 20:52:18 +0900

    微調整

--- script.js
+++ script.js
@@ -405,9 +405,8 @@ document.getElementById("upload_button").onclick = () => {
       <div id="uploadArea">
       <h3><ruby><rb>作</rb><rt>つく</rt></ruby>った<ruby><rb>絵</rb><rt>え</rt></ruby>をアップロードしよう！</h3>  
       <div><ruby><rb>絵</rb><rt>え</rt></ruby>をアップロードするとホームページ<ruby><rb>内</rb><rt>ない</rt></ruby>を<ruby><rb></div>
-      <div>絵</rb><rt>え</rt></ruby>が<ruby><rb>泳</rb><rt>およ</rt></ruby>ぎます。<div>
+      <div><rb>絵</rb><rt>え</rt></ruby>が<ruby><rb>泳</rb><rt>およ</rt></ruby>ぎます。<div>
       <div>ほかの人にも<ruby><rb>自分</rb><rt>じぶん</rt></ruby>の<ruby><rb>作品</rb><rt>さくひん</rt></ruby>をじまんしよう！</div>
-        <div><img src=${sendData} height="360px" width="640px" id="imgArea"></div>
         <div>
           <input type="text" placeholder="ニックネーム" id="uploadName" size="20">
           <button id="uploadPost">アップロード</button>
